897a21af8f6689cc6853e69e512740c11ae541f2
madhaven <37440750+madhaven@users.noreply.github.com>
2025-03-07 21:36:55 +0530

    refactor

--- Client/Bullet.cpp
+++ Client/Bullet.cpp
@@ -3,22 +3,19 @@
 
 void Bullet::Render(Graphics* graphics)
 {
-	if (isAlive)
-	{
-		graphics->DrawEllipse(position.x, position.y, 5, color);
-	}
+	if (!isAlive) { return; }
+	graphics->DrawEllipse(position.x, position.y, 5, color);
 }
 
 void Bullet::Update()
 {
-	if (isAlive)
-	{
-		if (position.x > playableArea.right) { isAlive = FALSE; }
-		if (position.y > playableArea.bottom) { isAlive = FALSE; }
-		if (position.x < 0) { isAlive = FALSE; }
-		if (position.y < 0) { isAlive = FALSE; }
+	if (!isAlive) { return; }
+	
+	if (position.x > playableArea.right) { isAlive = FALSE; }
+	if (position.y > playableArea.bottom) { isAlive = FALSE; }
+	if (position.x < 0) { isAlive = FALSE; }
+	if (position.y < 0) { isAlive = FALSE; }
 
-		position.x += cos(angle) * speed;
-		position.y += sin(angle) * speed;
-	}
+	position.x += cos(angle) * speed;
+	position.y += sin(angle) * speed;
 }
--- Client/Player.cpp
+++ Client/Player.cpp
@@ -74,45 +74,48 @@ bool Player::GetNextBullet(const Bullet& bullet)
 
 void Player::OnWinEvent(const UINT msg, const WPARAM w_param, const LPARAM l_param)
 {
-	if (msg == WM_MOUSEMOVE)
+	switch (msg)
 	{
+	case WM_MOUSEMOVE:
 		pos_aim_.x = GET_X_LPARAM(l_param);
 		pos_aim_.y = GET_Y_LPARAM(l_param);
-	}
-	else if (msg == WM_KEYDOWN && w_param == VK_SPACE && trigger_released_)
-	{
-		Fire();
-	}
-	else if (msg == WM_LBUTTONDOWN)
-	{
+		break;
+	case WM_LBUTTONDOWN:
 		Fire();
 		trigger_released_ = TRUE;
-	}
-	else if (msg == WM_KEYDOWN && w_param == 0x52)	// R => 0x52
-	{
-		ResetBullets();
-	}
-	else if (msg == WM_KEYUP)
-	{
+		break;
+	case WM_KEYDOWN:
+		if (w_param == VK_SPACE && trigger_released_)
+		{
+			Fire();
+		}
+		else if (w_param == 0x52) // R => 0x52
+		{
+			ResetBullets();
+		}
+		else
+		{
+			if (w_param == VK_UP || w_param == 0x57) { key_direction_.y = -1; }
+			if (w_param == VK_DOWN || w_param == 0x53) { key_direction_.y = 1; }
+			if (w_param == VK_RIGHT || w_param == 0x44) { key_direction_.x = 1; }
+			if (w_param == VK_LEFT || w_param == 0x41) { key_direction_.x = -1; }
+		}
+		break;
+	case WM_KEYUP:
 		if (w_param == VK_SPACE) { trigger_released_ = TRUE; }
 		if (w_param == VK_UP || w_param == 0x57) { key_direction_.y = 0; }
 		if (w_param == VK_DOWN || w_param == 0x53) { key_direction_.y = 0; }
 		if (w_param == VK_RIGHT || w_param == 0x44) { key_direction_.x = 0; }
 		if (w_param == VK_LEFT || w_param == 0x41) { key_direction_.x = 0; }
 
-	    // Exit game if 'Q' or 'ESC' is pressed
+		// Exit game if 'Q' or 'ESC' is pressed
 		if (w_param == 0x51 || w_param == 0x1B)
 		{
-		    PostQuitMessage(0);
+			PostQuitMessage(0);
 		}
+		break;
+	default:;
 	}
-    else if (msg == WM_KEYDOWN)
-    {
-        if (w_param == VK_UP || w_param == 0x57) { key_direction_.y = -1; }
-        if (w_param == VK_DOWN || w_param == 0x53) { key_direction_.y = 1; }
-        if (w_param == VK_RIGHT || w_param == 0x44) { key_direction_.x = 1; }
-        if (w_param == VK_LEFT || w_param == 0x41) { key_direction_.x = -1; }
-    }
 }
 
 void Player::Update()
