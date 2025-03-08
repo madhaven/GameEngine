3d921154029a4244d8c61c2eb753419bd5939a30
madhaven <37440750+madhaven@users.noreply.github.com>
2025-03-08 15:34:59 +0530

    add vector class

---
+++ Client/Utilities/Vector2D.cpp
@@ -0,0 +1,99 @@
+﻿#include "Vector2D.h"
+#include "cmath"
+
+Vector2D::Vector2D()
+{
+    this->x = 0;
+    this->y = 0;
+}
+
+Vector2D::Vector2D(float x, float y)
+{
+    this->x = x;
+    this->y = y;
+}
+
+Vector2D::Vector2D(const Vector2D& vector)
+{
+    this->x = vector.x;
+    this->y = vector.y;
+}
+
+float Vector2D::magnitude() const
+{
+    if (this->x == 0 && this->y == 0)
+        return 0;
+    return std::sqrt(x * x + y * y);
+}
+
+Vector2D Vector2D::operator+(const Vector2D& rhs) const
+{
+    return {this->x + rhs.x, this->y + rhs.y};
+}
+
+Vector2D Vector2D::operator+(const float rhs) const
+{
+    return {this->x + rhs, this->y + rhs};
+}
+
+Vector2D Vector2D::operator+=(const Vector2D& rhs)
+{
+    this->x += rhs.x;
+    this->y += rhs.y;
+    return *this;
+}
+
+Vector2D Vector2D::operator+=(const float rhs)
+{
+    this->x += rhs;
+    this->y += rhs;
+    return *this;
+}
+
+Vector2D Vector2D::operator-(const Vector2D& rhs) const
+{
+    return {this->x - rhs.x, this->y - rhs.y};
+}
+
+Vector2D Vector2D::operator-(const float rhs) const
+{
+    return {this->x + rhs, this->y + rhs};
+}
+
+Vector2D Vector2D::operator-=(const Vector2D& rhs)
+{
+    this->x -= rhs.x;
+    this->y -= rhs.y;
+    return *this;
+}
+
+Vector2D Vector2D::operator-=(const float rhs)
+{
+    this->x -= rhs;
+    this->y -= rhs;
+    return *this;
+}
+
+Vector2D Vector2D::operator*(const float rhs) const
+{
+    return {this->x * rhs, this->y * rhs};
+}
+
+Vector2D Vector2D::operator*=(const float rhs)
+{
+    this->x *= rhs;
+    this->y *= rhs;
+    return *this;
+}
+
+Vector2D Vector2D::operator/(const float rhs) const
+{
+    return {this->x / rhs, this->y / rhs};
+}
+
+Vector2D Vector2D::operator/=(const float rhs)
+{
+    this->x /= rhs;
+    this->y /= rhs;
+    return *this;
+}
